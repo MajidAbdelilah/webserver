ae0679792ebbf76b2f58dda5ac62ae6c790a4698
Yassine Mabsoute <ymabsout@e3r8p12.1337.ma>
2024-07-30 13:19:45 +0100

    added a little client test that display the ip and the port used for connection

--- Server.cpp
+++ Server.cpp
@@ -18,23 +18,31 @@ int Server::run(){
                   "Server: webserver-c\r\n"
                   "Content-type: text/html\r\n\r\n"
                   "<html>hello, world</html>\r\n";
+
     hostAddr.sin_family = AF_INET;
     hostAddr.sin_port = htons(PORT); // uses default port
     hostAddr.sin_addr.s_addr = htonl(INADDR_ANY); // takes any address
 
+    struct sockaddr_in client_sock; // client sockaddr_in
+    int client_addrlen = sizeof(client_sock); //client sockaddr_in length
+
     if (bind(_Tcpsocketfd, (struct sockaddr *) &hostAddr, sizeof(hostAddr)) == -1)
         throw("Bind failure");
-    if (listen(_Tcpsocketfd, 10) == -1)
+    if (listen(_Tcpsocketfd, SOMAXCONN) == -1)
         throw("listen failure");
 
     int serversize = sizeof(hostAddr);
     // handle client connections 
-    while (1){
+    while (1) {
         int clientfd = accept(_Tcpsocketfd, (struct sockaddr *) &hostAddr, (socklen_t *)&serversize);
         if (clientfd < 0)
             throw ("Clientfd error");
         std::cout << "connection accepted" << std::endl;
         write(clientfd, resp , strlen(resp));
+        int _socknameclient = getsockname(clientfd, (struct sockaddr *)&client_sock, (socklen_t *)&client_addrlen );
+        if (_socknameclient == -1)
+            throw ("Clientname error");
+        std::cout  << "[ " << inet_ntoa(client_sock.sin_addr) << " , " << ntohs(client_sock.sin_port) << " ]" << std::endl;
     }
 }
 
